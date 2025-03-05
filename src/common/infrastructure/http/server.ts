c3f6c09d58790dc281ba2e4804e804b6e93baad2
Jessica Santos <jel_008@hotmail.com>
2025-03-05 16:20:59 -0300

    Configuration environment variables

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,9 +1,6 @@
+import { env } from "../env";
 import { app } from "./app";
 
-
-const port = 3333;
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+app.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT}`)
 }) 
