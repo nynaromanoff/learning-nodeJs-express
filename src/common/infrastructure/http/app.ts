75aacd3e136d635d90e7f9ab98167607745bd94b
Jessica Santos <jel_008@hotmail.com>
2025-03-05 16:48:55 -0300

    Configuration Swagger on project

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -1,12 +1,28 @@
 import express from 'express'
 import cors from 'cors'
-import { routes } from './routes';
-import { erroHandler } from './middlewares/errorHandler';
+import { routes } from './routes'
+import { erroHandler } from './middlewares/errorHandler'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0'
+    },
+  },
+  apis: ['./src/modules/**/infra/http/routes/*.ts'],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(routes)
 app.use(erroHandler)
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -3,4 +3,5 @@ import { app } from "./app";
 
 app.listen(env.PORT, () => {
     console.log(`Server running on port ${env.PORT}`)
+    console.log('API docs available at GET /docs')
 }) 
